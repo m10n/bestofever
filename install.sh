0bfb325444851c8d1268d8f4577c92b5053d0636
Morgan Missen <morganmissen@gmail.com>
2014-03-05 18:23:36 -0800

    Fixes to install script.

--- install.sh
+++ install.sh
@@ -2,26 +2,28 @@
 mkdir -p ./tmp
 cd ./tmp
 
-echo 'Downloading get-pip.py'
+echo '===== Downloading get-pip.py'
 curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 
-echo 'Running get-pip.py'
+echo '===== Running get-pip.py (enter your password when prompted)'
 sudo python ./get-pip.py
 
-echo 'Installing Django'
+echo '==== Installing Django'
 sudo pip install Django==1.3.3
 
-echo 'Installing python mysql library'
+echo '==== Installing python mysql library'
 sudo pip install mysql_python
 
-echo 'Installing markdown'
+echo '==== Installing markdown'
 sudo pip install markdown
 
-echo 'Installing html5lib'
+echo '==== Installing html5lib'
 sudo pip install html5lib
 
 cd ..
 
-echo 'Syncing DB'
-python manage.py syncdb
+echo '==== Syncing DB'
+cp db.sqlite3.init db.sqlite3
 
+echo 'Done!'
+echo '(To run a development server: python manage.py runserver)'
