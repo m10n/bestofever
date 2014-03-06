39a8a0417853c494dc81695da6a2d7c74dd760a9
Benjy <benjyw@gmail.com>
2014-03-05 19:39:08 -0800

    Show user avatar in sidebar.

--- forum/templatetags/general_sidebar_tags.py
+++ forum/templatetags/general_sidebar_tags.py
@@ -15,25 +15,16 @@ def recent_awards():
     return {'awards': Award.objects.order_by('-awarded_at')[:settings.RECENT_AWARD_SIZE]}
 
 @register.inclusion_tag('sidebar/user_blocks.html')
-def sidebar_upper():
+def sidebar_upper(user=None):
     return {
         'show': settings.SIDEBAR_UPPER_SHOW,
         'content': static_content(settings.SIDEBAR_UPPER_TEXT, settings.SIDEBAR_UPPER_RENDER_MODE),
         'wrap': not settings.SIDEBAR_UPPER_DONT_WRAP,
-        'blockid': 'sidebar-upper'
-    }
-
-@register.inclusion_tag('sidebar/user_blocks.html')
-def sidebar_lower():
-    return {
-        'show': settings.SIDEBAR_LOWER_SHOW,
-        'content': static_content(settings.SIDEBAR_LOWER_TEXT, settings.SIDEBAR_LOWER_RENDER_MODE),
-        'wrap': not settings.SIDEBAR_LOWER_DONT_WRAP,
-        'blockid': 'sidebar-lower'
+        'blockid': 'sidebar-upper',
+        'view_user': user
     }
 
 @register.inclusion_tag('sidebar/recent_tags.html')
 def recent_tags():
     return {'tags': Tag.active.order_by('-id')[:settings.RECENT_TAGS_SIZE]}
 
-    
