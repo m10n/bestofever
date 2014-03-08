b8c937e2d84b936239b989b745f302cb3622bcac
Morgan Missen <morganmissen@gmail.com>
2014-03-07 17:50:08 -0800

    Installed Bootstrap, Pipeline,
    django-twitter-bootstrap 3.1.0
    Provides a Django app whose static folder contains Twitter Bootstrap
    assets
    https://pypi.python.org/pypi/django-twitter-bootstrap/
    
    Crispy-forms
    http://django-crispy-forms.readthedocs.org/en/d-0/index.html
    
    and
    
    Pipeline
    https://github.com/cyberdelia/django-pipeline
    
    with a LESS compiler

--- settings.py
+++ settings.py
@@ -112,6 +112,7 @@ MODULE_LIST = filter(lambda m: getattr(m, 'CAN_USE', True), [
                           ) for m in MODULE_LIST]
 
 
+
 INSTALLED_APPS = INSTALLED_APPS or []
 INSTALLED_APPS.extend([
     'django.contrib.auth',
@@ -124,7 +125,46 @@ INSTALLED_APPS.extend([
     'django.contrib.markup',
     'django.contrib.messages',
     'forum',
+    'crisp_forms',
+    'twitter_bootstrap',
+    'django.contrib.staticfiles',
 ])
+
+PIPELINE_CSS = {
+    'bootstrap': {
+        'source_filenames': (
+            'twitter_bootstrap/less/bootstrap.less',
+        ),
+        'output_filename': 'css/b.css',
+        'extra_context': {
+            'media': 'screen,projection',
+        },
+    },
+}
+
+PIPELINE_JS = {
+    'bootstrap': {
+        'source_filenames': (
+          'twitter_bootstrap/js/transition.js',
+          'twitter_bootstrap/js/modal.js',
+          'twitter_bootstrap/js/dropdown.js',
+          'twitter_bootstrap/js/scrollspy.js',
+          'twitter_bootstrap/js/tab.js',
+          'twitter_bootstrap/js/tooltip.js',
+          'twitter_bootstrap/js/popover.js',
+          'twitter_bootstrap/js/alert.js',
+          'twitter_bootstrap/js/button.js',
+          'twitter_bootstrap/js/collapse.js',
+          'twitter_bootstrap/js/carousel.js',
+          'twitter_bootstrap/js/affix.js',
+        ),
+        'output_filename': 'js/b.js',
+    },
+}
+
+PIPELINE_COMPILERS = (
+  'pipeline.compilers.less.LessCompiler',
+)
 
 if DEBUG:
     try:
